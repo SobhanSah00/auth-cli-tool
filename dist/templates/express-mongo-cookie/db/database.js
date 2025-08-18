805bdafa0cf0939522dc21d380fad01d5af403cc
sobhan9012 <sobhansahoo2000@gmail.com>
2025-08-19 00:14:53 +0530

    temple-2 , mongo cookie complete

---
+++ dist/templates/express-mongo-cookie/db/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const DB_NAME = "auth-mongo-cookie";
+export const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log(`\n MONGODB CONNECTED !! DB HOST: ${connectionInstance.connection.host}`);
+        // console.log(connectionInstance)
+    }
+    catch (error) {
+        console.log("MONGODB CONNECTION ERROR", error);
+        process.exit(1);
+    }
+};
